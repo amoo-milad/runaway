cf68c967c821e776c94018b5c1b7c98d4da3e826
milad <milad.khaari@gmail.com>
2019-07-26 19:37:49 +0430

    modified 'runaway/urls'

--- runaway/urls.py
+++ runaway/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('critic/', include(critic.urls)),
+    path('critic/', include('critic.urls')),
     path('admin/', admin.site.urls),
 ]
