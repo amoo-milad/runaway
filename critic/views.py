0de7219750b9eb86f861cba0acef3831bd59a030
milad <milad.khaari@gmail.com>
2019-07-29 21:43:26 +0430

    used render instead of httpResponse

--- critic/views.py
+++ critic/views.py
@@ -1,17 +1,14 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    template = loader.get_template('critic/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'critic/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
