49a3e8ae3abdc351b0a0179481cbde0ddc8c797b
milad <milad.khaari@gmail.com>
2019-07-29 17:31:28 +0430

    added Details/Results/Vote

--- critic/urls.py
+++ critic/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    # ex: /critic/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    
+    # ex: /critic/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    
+    # ex: /critic/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
