4e74c6ba4ba63d58e962169b946f921efcc4861c
milad <milad.khaari@gmail.com>
2019-07-29 22:32:41 +0430

    changed detail: added 'get_object_or_404()'

--- critic/views.py
+++ critic/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
+# from django.http import HttpResponse
+# from django.http import Http404
 from .models import Question
 
 def index(request):
@@ -11,7 +12,8 @@ def index(request):
     return render(request, 'critic/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'critic/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
