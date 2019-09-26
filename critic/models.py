64e803d58140e2889b525fdccde6951c5717e989
milad <milad.khaari@gmail.com>
2019-09-26 05:00:59 +0330

    cleaning, reWriting

--- critic/models.py
+++ critic/models.py
@@ -41,16 +41,16 @@ class Movie(models.Model):
         return self.title
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+# class Author(models.Model):
+#     name = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.author
+#     def __str__(self):
+#         return self.author
 
 
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    # author = models.ForeignKey(Author, on_delete=models.CASCADE)
     review_text = models.CharField(max_length=2000)
     pub_date = models.DateTimeField('publish date')
     author_name = models.CharField(max_length=100)
